8b132b3d018013bb22f92e3065203207e9dfb83c
westcrew3 <93713828+westcrew3@users.noreply.github.com>
2022-04-05 07:47:17 -0400

    Add files via upload

---
+++ maththing.py
@@ -0,0 +1,5 @@
+print("this is a adding machine")
+first1= int(input("what's your first number"))
+sec2= int(input("what's your second number"))
+sum =first1+sec2
+print("the sum of your two number are",sum)
